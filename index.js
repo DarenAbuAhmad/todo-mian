b8db3cc70e6800d2f81c35993e863035cee48c3d
DarenAbuAhmad <Daren.2004d@gmail.com>
2026-01-30 09:17:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const path = require('path'); // ✅ أولًا تعريف pa
+const path = require('path'); // ✅ أولًا تعريف p
 require('dotenv').config({ path: path.join(__dirname, '.env') }); // ✅ ثم dotenv
 const express = require('express');
 const db = require('./config/db_config');
