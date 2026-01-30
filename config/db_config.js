605220621a415e59b844f8e78766c648abe65299
DarenAbuAhmad <Daren.2004d@gmail.com>
2026-01-30 09:11:45 +0200

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,        
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, // ✅
+  password: process.env.DB_PASSWORD, // 
   port: process.env.DB_PORT,
   dateStrings: true 
 });
