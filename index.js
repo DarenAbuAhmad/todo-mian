39e57fb4d0baf1ed9a92fc1cba1f0356504044ce
DarenAbuAhmad <Daren.2004d@gmail.com>
2026-01-30 09:19:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path');  
-require('dotenv').config({ path: path.join(__dirname, '.env') }); // ✅ ثم
+require('dotenv').config({ path: path.join(__dirname, '.env') }); // ثم
 const express = require('express');
 const db = require('./config/db_config');
 const cookies = require('cookie-parser');
