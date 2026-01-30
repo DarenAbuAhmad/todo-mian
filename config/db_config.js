3ae379f2f80b189ddaa49a957fc71d967624eb60
DarenAbuAhmad <Daren.2004d@gmail.com>
2026-01-30 09:11:23 +0200

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,        // ✅
+  host: process.env.DB_HOST,        // 
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD, // ✅
