fced64ce26dee253fc06783dadb0eda6a48894bd
DarenAbuAhmad <Daren.2004d@gmail.com>
2026-01-30 10:16:08 +0200

    Update auth_R.js

--- routes/auth_R.js
+++ routes/auth_R.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-........
+....
 const {valuesToAdd,encrypPass,valuesToLogin} = require('../middelware/auth_MID.js');
 const {register,login,createJwt} = require('../controller/auth_C.js');
 
